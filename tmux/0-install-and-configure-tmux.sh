9c15aad606b80818aac3729c5a9583d6e78b03c6
Kirill Tolkachev <lavcraft@gmail.com>
2018-02-16 00:07:01 +0300

    Add hidutil initial configuration

--- tmux/0-install-and-configure-tmux.sh
+++ tmux/0-install-and-configure-tmux.sh
@@ -12,4 +12,7 @@ brew install caskroom/cask/karabiner-elements
 _install '~/.tmux.conf'
 ln -sfv "$PWD/tmux.conf" $HOME/.tmux.conf
 
+_install 'Configure caps-lock as prefix key by hidutil'
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000043}]}'
+
 popd
