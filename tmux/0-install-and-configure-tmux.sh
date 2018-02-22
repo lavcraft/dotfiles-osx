4acc1c4d31a97daf7d8a8f947dc34cf056797b98
Tolkachev Kirill <lavcraft@gmail.com>
2018-02-22 19:27:49 +0300

    Remove karabiner installation

--- tmux/0-install-and-configure-tmux.sh
+++ tmux/0-install-and-configure-tmux.sh
@@ -7,7 +7,6 @@ source $CURDIR/../.functions
 pushd $CURDIR
 
 _title 'Configure tmux'
-brew install caskroom/cask/karabiner-elements
 
 _install '~/.tmux.conf'
 ln -sfv "$PWD/tmux.conf" $HOME/.tmux.conf
